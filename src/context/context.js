4b96e7db8a1624974f701be48519680573825581
Mohamed H.Fakher <m7mdfa5er2811@gmail.com>
2021-10-03 22:08:25 +0200

    Setup ContextApi , Login Page

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext, useContext } from 'react';
 import mockUser from './mockData.js/mockUser';
 import mockRepos from './mockData.js/mockRepos';
 import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = createContext();
+
+// Provider , Comsumer
+
+const GithubProvider = ({ children }) => {
+  return (
+    <GithubContext.Provider value={{ value: 'hi' }}>
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+
+//Custom hook
+export const useGlobalContext = () => {
+  return useContext(GithubContext);
+}
+
+export { GithubProvider, GithubContext }
